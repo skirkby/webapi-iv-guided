8a84cb61a7567f16fceebfa816d9181954a41656
Sean Kirkby <sean.kirkby@niceincontact.com>
2019-11-06 23:44:37 -0700

    notes

--- api/server.js
+++ api/server.js
@@ -11,6 +11,19 @@ server.use(express.json());
 server.get('/', async (req, res) => {
   try {
     const shoutouts = await db('shoutouts');
+    // This "message of the day" variable was created to demonstrate creating
+    // environment variables in the Heroku platform. You can define environment
+    // variables that Heroku will create in the OS environment is makes for your
+    // app by clicking on the "Settings" tab for your app in Heroku.
+    //
+    // Our app will not be retrieving the PORT variable from a definition in
+    // this way... Heroku automatically creates the PORT environment variable
+    // for us (we don't need to tell it to). But there are times when we still
+    // want other configuration environment variables that our app can use. And
+    // since we don't have access to the shell environment before our
+    // application is executed by Heroku, we need to define the variables we
+    // want Heroku to create and export for our app using this feature
+    // (Settings). 
     const motd = process.env.MOTD || 'Hi there!';
 
     res.status(200).json({ message: motd, shoutouts });
